cde1253a898e65a1ced3a7921d8be7cdaf272a75
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-03-26 06:20:25 +0530

    install draw.io

--- c++ DSA/10 . Pointers/intex.cpp
+++ c++ DSA/10 . Pointers/intex.cpp
@@ -14,9 +14,10 @@ int a = 5;
 // cout << &a << endl;
     
 int arr[] = {1,2,3,4,5};
-cout << *arr << endl;
-cout << *(arr+1) << endl;
-cout << *(arr+2) << endl;
-cout << *(arr+3) << endl;
+int* ptr2;
+int* ptr1 = ptr2;
+cout << ptr1 << endl;
+cout << ptr2 << endl;
+cout << (ptr1 < ptr2) << endl;
     return 0;
 }
