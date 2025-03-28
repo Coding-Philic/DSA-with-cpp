de3b6d4d4cace835ad7da6d1bafd024be212fd98
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-03-28 07:29:11 +0530

    less optimaized way to solve the proble

--- c++ DSA/12. Peak index in mountain array/index.cpp
+++ c++ DSA/12. Peak index in mountain array/index.cpp
@@ -5,34 +5,23 @@ using namespace std;
 int main() {
     vector<int> nums = {0,3,8,9,10,2};
     int n = nums.size();
-    if(n==1){
-        return nums[0];
-    }
-    int st = 0, end = n-1;
+    int st= 0, end = n-1;
+    int i = 0;
     while(st <= end){
         int mid = st + (end-st)/2;
-          if(mid == 0 && nums[0]!=nums[1]){
-            return nums[mid];
-        };
-        if(mid == n-1&& nums[n-1] !=nums[mid-2] ){
-            return nums[mid];
+        if(nums[mid-1] <= nums[mid] && nums[mid] >= nums[mid+1]){
+            cout << mid << endl;
         }
-        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) {
-            return nums[mid];
+        if(nums[mid-1] <= nums[mid] && nums[mid] <= nums[mid+1]){
+            st = mid + 1;
+        }else if(nums[mid-1] >= nums[mid] && nums[mid] >= nums[mid+1]){
+            end = mid - 1;
+        }else{
+            cout << mid << endl;
+            return mid;
         }
-        if(mid%2==0){
-        if(nums[mid] == nums[mid-1]){
-            end = mid-1;
-        }else{
-            st = mid+1;
-        }   
-        }else{
-            if(nums[mid] == nums[mid-1]){
-            st = mid+1;
-        }else{
-            end = mid-1;
-        } 
-        }
+        
     }
+    cout << -1 << endl;
     return -1;
 }
