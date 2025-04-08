605f555647da9a115ec3e67cdfe0218fc750171b
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-04-08 21:28:32 +0530

    learn a new type of cin there is a delim which stop geting input in specific words or sysmbole what ever your give

--- c++ DSA/17. string part 1/index.cpp
+++ c++ DSA/17. string part 1/index.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     char str[100];
     cout << "Enter a string: "; 
-   cin.getline(str, 100); // read a string with spaces
+   cin.getline(str, 100, '$'); // read a string with spaces
    
     cout << str << endl; // constant pointers
     return 0;
