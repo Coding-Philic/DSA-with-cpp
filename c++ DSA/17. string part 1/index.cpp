29b13c085258f93729f8f6f14ce716a8b758b07b
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-04-09 21:27:07 +0530

    add for each loop

--- c++ DSA/17. string part 1/index.cpp
+++ c++ DSA/17. string part 1/index.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main() {
-    char str[100];
+    char str[6];
     cout << "Enter a string: "; 
-   cin.getline(str, 100, '$'); // read a string with spaces
-   
+   cin.getline(str, 6, '$'); // read a string with spaces
+   for(char ch : str){
+    cout << ch << " ";
+   }
     cout << str << endl; // constant pointers
     return 0;
 }
