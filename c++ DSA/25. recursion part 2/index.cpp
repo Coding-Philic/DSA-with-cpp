66be63304098a1772cc9adfcadbc2a1b563a1045
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-04-27 21:23:39 +0530

    this is a code how to find the arr is soted or not

--- c++ DSA/25. recursion part 2/index.cpp
+++ c++ DSA/25. recursion part 2/index.cpp
@@ -3,21 +3,19 @@
 #include <vector>
 using namespace std;
 int soted(vector<int>arr,int n, int i ){
-   
-    
-    if(i == n){
+     if(i == n){
         return 1;
     }
     if(arr[i]>=arr[i-1]){
-        soted(arr,n,(i+1));
+       return soted(arr,n,(i+1));
     }else{
         return 0;
     }
-    return 1;
+    
     }
 int main() {
     // Write C++ code here
-     vector<int>arr = {2,4,5,16,70,155,1000};
+     vector<int>arr = {2,4,5,16,300,155,1000};
 int n = arr.size();
 cout << soted(arr,n,1);
     return 0;
