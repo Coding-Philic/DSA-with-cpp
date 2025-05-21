9719177cffffb65876bf36bb23df475f9bcd5ccd
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-05-21 22:29:50 +0530

    add one more leadcode question this question number is

---
+++ c++ DSA/29. Middle of a linked list/index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // ll*slow = head;
+    // ll*fast = head;
+
+    // while(fast != NULL && fast->next != NULL){
+    //     slow = slow->next;
+    //     fast = fast->next->next;
+    // }
+    // return slow;
+}
