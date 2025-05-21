4cc93b8518d4e57c19176e94071df06dbcf8a217
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-05-21 23:14:40 +0530

    this is a code of how to find cycle in linked list

---
+++ c++ DSA/30.Linked list cycle/index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // this is a problem number 141 on leadcode;
+  
+    // ll*slow= head;
+    // ll*fast= head;
+
+
+    // while (fast != NULL && fast->next != NULL){
+    //     slow = slow->next;
+    //     fast = fast->next->next;
+    //     if(fast == slow){
+    //         return true;
+    //     }
+    // }
+    // return false;
+}
