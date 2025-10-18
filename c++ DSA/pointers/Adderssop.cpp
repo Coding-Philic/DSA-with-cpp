47a927b68d12989e250d2628a3309353d493b621
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-10-18 08:17:01 -0700

    add pointer to pointer

--- c++ DSA/pointers/Adderssop.cpp
+++ c++ DSA/pointers/Adderssop.cpp
@@ -4,7 +4,22 @@ using namespace std;
 int main(){
     int a = 10;
     int *ptr = &a;
-    cout << ptr << endl;
+
+    int **ptr2 = &ptr;
+
+    cout << ptr2 << endl;
+    cout << &ptr << endl;
+
+    // float pi = 3.14;
+    // float *prt1 = &pi;
+
+
+
+    // cout << sizeof(ptr) << endl;
+    // cout << sizeof(prt1) << endl;
+
+    // cout << prt1 << endl;
+    // cout << ptr << endl;
 
     return 0;
 }
