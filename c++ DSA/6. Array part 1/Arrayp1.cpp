7894f10097b62af1950911194d9e5a335a786963
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-10-19 22:30:02 -0700

    add input and output system

--- c++ DSA/6. Array part 1/Arrayp1.cpp
+++ c++ DSA/6. Array part 1/Arrayp1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int marks[] = {1,2,3,4,5,6};
+    // int marks[] = {1,2,3,4,5,6};
     // cout << marks[0] << endl;
     // cout << marks[1] << endl;
     // cout << marks[2] << endl;
@@ -11,6 +11,32 @@ int main(){
     // cout << marks[4] << endl;
     // cout << marks[70] << endl;
 
-    cout << sizeof(marks) << endl;
+    // cout << sizeof(marks) << endl;
+
+
+
+    //output & input Array
+
+    // int arr[5] = {2,34,2,4,6};
+
+    // for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
+    // {
+    //    cout << arr[i] << endl;
+    // }
+    
+
+    // input array
+
+    int arr[5];
+
+    for(int i = 0; i <sizeof(arr)/sizeof(int); i++){
+        cin >> arr[i];
+        cout << endl;
+    }
+    for(int i = 0; i <sizeof(arr)/sizeof(int); i++){
+        cout << "student " <<  i << " marks = " <<arr[i] << endl;
+    }
+    
+
     return 0;
 }
