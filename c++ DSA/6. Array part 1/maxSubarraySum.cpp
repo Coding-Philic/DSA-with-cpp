411081a4aaea7e066c7a7c81be4ebae0513c7c41
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-10-23 08:47:05 -0700

    optimized by this using kadane's algorithm

--- c++ DSA/6. Array part 1/maxSubarraySum.cpp
+++ c++ DSA/6. Array part 1/maxSubarraySum.cpp
@@ -4,17 +4,14 @@ using namespace std;
 
 int maxSubarray(int *arr, int n){
     int sum = INT32_MIN;
-    int temp = INT32_MIN;
+    int temp = 0;
     for (int i = 0; i < n; i++)
     {
+      temp += arr[i];
+      sum = max(temp,sum);
+      if(temp<0){
         temp = 0;
-
-        for (int j = i; j < n; j++)
-        {
-         temp += arr[j];
-         sum = max(sum,temp);
-         }
-        cout << endl;
+      }
         
     }
     cout << "max sum is : "<< sum << endl;
@@ -22,7 +19,7 @@ return 0;
 }
 
 int main() {
-    int arr[] = {2,-3,6,-5,4,2,2};
+    int arr[] = {2,-3,6,-5,4,2};
     int n = sizeof(arr) / sizeof(int);
     maxSubarray(arr, n);
     return 0;
