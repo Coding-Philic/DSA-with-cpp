17a7afcf675a0fdcdd7fd4625936e7c0414197bb
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-10-23 08:38:48 -0700

    do a optimization in this max subbarray sum

--- c++ DSA/6. Array part 1/maxSubarraySum.cpp
+++ c++ DSA/6. Array part 1/maxSubarraySum.cpp
@@ -7,17 +7,13 @@ int maxSubarray(int *arr, int n){
     int temp = INT32_MIN;
     for (int i = 0; i < n; i++)
     {
+        temp = 0;
+
         for (int j = i; j < n; j++)
         {
-          for (int k = i; k <= j; k++)
-          {
-            cout << arr[k] ;
-            temp+=arr[k];
-          }
-        sum = max(sum,temp);
-          temp = 0;
-            cout << ",";
-        }
+         temp += arr[j];
+         sum = max(sum,temp);
+         }
         cout << endl;
         
     }
@@ -26,7 +22,7 @@ return 0;
 }
 
 int main() {
-    int arr[] = {2,-3,6,-5,4,2};
+    int arr[] = {2,-3,6,-5,4,2,2};
     int n = sizeof(arr) / sizeof(int);
     maxSubarray(arr, n);
     return 0;
