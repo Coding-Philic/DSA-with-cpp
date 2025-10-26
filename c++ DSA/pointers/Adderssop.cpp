42fc23a98d3f2dda21c2340c4d17a91017d43c4d
Coding-Philic <Adnan.Khan63945@gmail.com>
2025-10-26 03:09:37 -0700

    Add a pointers

--- c++ DSA/pointers/Adderssop.cpp
+++ c++ DSA/pointers/Adderssop.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// void changeA (int *a){
+//     *a = 20;
+//     cout << *a << endl;
+// }
+
 int main(){
     int a = 10;
-    int *ptr = &a;
+    int &b = a;
+    cout << b << endl;
+    // changeA(&a);
+    cout << a << endl;
+    cout << &a << endl;
+    cout << &b << endl;
+    b = 30;
+    cout << a << endl;
+    cout << b << endl;
 
-    int **ptr2 = &ptr;
 
-    cout << ptr2 << endl;
-    cout << &ptr << endl;
-    cout << *ptr << endl;
+
+    // int *ptr = &a;
+
+    // int **ptr2 = &ptr;
+
+    // cout << ptr2 << endl;
+    // cout << &ptr << endl;
+    // cout << *ptr << endl;
 
     // float pi = 3.14;
     // float *prt1 = &pi;
