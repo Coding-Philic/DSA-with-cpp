d4fa30df1825f9a758d92cddb8589611a2e37fbf
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-01-04 19:55:30 +0530

    start a heap:

---
+++ c++ DSA/Heap/push.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Heap{
+    vector<int> vec;
+public:
+
+    void push(int val){
+        vec.push_back(val);
+
+        int x = vec.size()-1;
+        int parI = (x-1)/2;
+
+        while(parI >= 0 && vec[x] > vec[parI]){
+            swap(vec[x],vec[parI]);
+            x = parI;
+            parI = (x-1)/2;
+        }
+    }
+
+    void pop(){
+
+    }
+
+    int top(){
+        int x = vec[0];
+        return x;
+    }
+
+    bool empty(){
+        return vec.size() == 0;
+    }
+};
+
+int main(){
+    Heap heap ;
+    heap.push(10);
+    heap.push(1);
+    heap.push(100);
+
+    cout << heap.top() << endl;
+    return 0;
+}
