2cbeb75831e488eabe776a4e8dabcf6489cbfdd1
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-01-22 22:34:34 +0530

    max subarray sum optimization

--- c++ DSA/revision/revision.cpp
+++ c++ DSA/revision/revision.cpp
@@ -2,20 +2,17 @@
 using namespace std;
 
 int main(){
-int arr[] = {1,2,3,4,5};
+int arr[] = {2,-3,6-5,4,2,-1,5};
 int maxsum = 0;
 int tempsum = 0;
 int n = sizeof(arr) / sizeof(int);
 for(int i = 0; i <n; i++){
-   for(int j = i; j < n; j++){
-        for(int k = i; k <= j; k++){
-            cout << arr[k];
-            tempsum += arr[k];
+  tempsum += arr[i];
+        if(tempsum<0){
+            tempsum = 0;
         }
         maxsum = max(maxsum,tempsum);
-        tempsum = 0;
-        cout << ", ";
-   }
+
    cout << endl;
 }
 cout << "max sum : " << maxsum << endl;
