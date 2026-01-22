df7d3b78b91e86107bbc20ee331fd2ce68b0b5a3
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-01-22 22:13:48 +0530

    solve max sum

--- c++ DSA/revision/revision.cpp
+++ c++ DSA/revision/revision.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 int main(){
-
-    int arr[] = {5,4,3,9,2};
-    int end = (sizeof(arr) / sizeof(int))-1;
-    int n = sizeof(arr) / sizeof(int);
-    int st = 0;
-    while(st<end){
-        swap(arr[st], arr[end]);
-        st++;
-        end--;
-    }
-
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-    
-
+int arr[] = {1,2,3,4,5};
+int maxsum = 0;
+int tempsum = 0;
+int n = sizeof(arr) / sizeof(int);
+for(int i = 0; i <n; i++){
+   for(int j = i; j < n; j++){
+        for(int k = i; k <= j; k++){
+            cout << arr[k];
+            tempsum += arr[k];
+        }
+        maxsum = max(maxsum,tempsum);
+        tempsum = 0;
+        cout << ", ";
+   }
+   cout << endl;
+}
+cout << "max sum : " << maxsum << endl;
     return 0;
 }
