8f60f88e73e764362e02e049377ea3d95de6bf37
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-01-22 18:20:45 +0530

    max value in arr

--- c++ DSA/revision/revision.cpp
+++ c++ DSA/revision/revision.cpp
@@ -10,8 +10,25 @@ int main(){
     for(int i = 0; i < l; i++){
         cin >> marks[i];
     }
-    for(int i =0; i <l; i ++){
-        cout << marks[i]<< endl;
+    int lar = marks[0];
+    int min = marks[0];
+    int midx = 0;
+    int minidx =0;
+    for(int i = 0; i <l; i++){
+        if(lar < marks[i]){
+            lar = marks[i];
+            midx = i;
+        }
+
+        if(min > marks[i]){
+            min = marks[i];
+            minidx = i;
+        }
+
     }
+    cout  << "maxidx : " << midx << endl;
+    cout << "max value : " << lar << endl;
+    cout << "minidx : " << minidx << endl;
+    cout << "min value : " << min << endl; 
     return 0;
 }
