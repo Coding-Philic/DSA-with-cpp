cce9d20f428eb77f2568bcff513bb4e8bc0f1fda
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-01-25 16:28:44 +0530

    trap a rain water

--- c++ DSA/revision/revision.cpp
+++ c++ DSA/revision/revision.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 using namespace std;
 
+
+
+
 int main(){
-int arr[] = {7,1,5,3,6,7};
-int n = sizeof(arr) / sizeof(int);
-int maxprof = 0;
-int currprof = 0;
-int idx1 = 0;
-int idx2 = 0;
+int height[] = {4,2,0,6,3,2,5};
+int n = sizeof(height) / sizeof(int);
+int trap = 0;
+int left[n];
+int right[n];
+int leftmax = 0;
+int rightmax =0; 
 for(int i = 0; i < n; i++){
-    for(int j = i+1; j <n; j++){
-        currprof = arr[j] - arr[i];
-        if(currprof>maxprof){
-            maxprof = max(currprof, maxprof);
-            idx1 = i;
-            idx2 = j;
-        }
-        currprof = 0;
+if(leftmax< height[i]){
+    leftmax = height[i];
+}
+if(leftmax == height[i]){
+    cout << i << endl;
+}
+left[i] = leftmax;
+}
+left[0] = INT16_MIN;
+for(int i = 0; i < n; i++){
+    cout << left[i] << endl;
+}
+
+
+
+for(int i = n-1; i >= 0; i--){
+if(rightmax< height[i]){
+    rightmax = height[i];
+}
+right[i] = rightmax;
+}
+right[n-1] = INT16_MIN;
+
+for(int i = 1; i < n-1; i++ ){
+    if(right[i]!=left[i]){
+    int bar = min(right[i], left[i]);
+    trap += bar - height[i];
     }
 }
-cout << maxprof << endl;
-cout << "(" << idx1 << "," << idx2 << ")" ;
-
+cout << "trap : " << trap << endl;
     return 0;
 }
