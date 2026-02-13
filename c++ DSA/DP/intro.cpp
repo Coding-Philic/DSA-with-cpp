8a47c6096e831aac859e31bd5fdc1f3139fe14c7
Coding-Philic <Adnan.Khan63945@gmail.com>
2026-02-13 22:37:24 +0530

    solve using dp

--- c++ DSA/DP/intro.cpp
+++ c++ DSA/DP/intro.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 
 int fib(int n){
@@ -8,8 +9,21 @@ int fib(int n){
     return fib(n-1)+fib(n-2);
 }
 
+
+int fibdp(int n, vector<int> &f){
+    if(n==0||n==1){
+        return n;
+    }
+    if(f[n]!=-1){
+        return f[n];
+    }
+    f[n] = fib(n-1)+fib(n-2);
+    return f[n];
+}
+
 int main(){
     int n = 6;
-    cout << fib(n) << endl;
+    vector<int> f(n+1, -1);
+    cout << fibdp(n, f) << endl;
     return 0;
 }
